4a9a9fd3e570c431bc3bf305c43d72c00796666d
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-02 22:31:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  
+ int a = 0;
+  cin >> a;
+  
+  if(numeriprimi((a/2),a)){
+    cout << "numero primo" << endl;
+  } else{
+    cout << "numero non primo" << endl;
+  }
+     
   return 0;
 }
